80b1ee224b6bb7f8c69a0822ba3811c802203dbe
VishnuSolleti <taraintern3@tarafirst.com>
2025-09-09 23:52:06 +0530

    Refactor project structure and update settings for deployment

--- Tara/settings.py
+++ Tara/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l$=d8!g9&#1+b(s)886ku0tv$0gc+$*6xe64hc1z(b&6et=ha$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
